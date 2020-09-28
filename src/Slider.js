1aea1fa0e60c0b2dc21d7164e6eb510d7a6fd07d
Chris Nguyen <christopher.nguyen13@gmail.com>
2020-09-28 10:41:40 +1000

    Remove prop types to support react-native-web

--- src/Slider.js
+++ src/Slider.js
@@ -11,11 +11,8 @@ import {
   PanResponder,
   View,
   Easing,
-  ViewPropTypes,
   Platform
 } from 'react-native';
-
-import PropTypes from 'prop-types';
 
 var TRACK_SIZE = 4;
 var THUMB_SIZE = 20;
@@ -51,135 +48,6 @@ var DEFAULT_ANIMATION_CONFIGS = {
 };
 
 export default class Slider extends PureComponent {
-  static propTypes = {
-    /**
-     * Initial value of the slider. The value should be between minimumValue
-     * and maximumValue, which default to 0 and 1 respectively.
-     * Default value is 0.
-     *
-     * *This is not a controlled component*, e.g. if you don't update
-     * the value, the component won't be reset to its inital value.
-     */
-    value: PropTypes.number,
-
-    /**
-     * If true the user won't be able to move the slider.
-     * Default value is false.
-     */
-    disabled: PropTypes.bool,
-
-    /**
-     * Initial minimum value of the slider. Default value is 0.
-     */
-    minimumValue: PropTypes.number,
-
-    /**
-     * Initial maximum value of the slider. Default value is 1.
-     */
-    maximumValue: PropTypes.number,
-
-    /**
-     * Step value of the slider. The value should be between 0 and
-     * (maximumValue - minimumValue). Default value is 0.
-     */
-    step: PropTypes.number,
-
-    /**
-     * The color used for the track to the left of the button. Overrides the
-     * default blue gradient image.
-     */
-    minimumTrackTintColor: PropTypes.string,
-
-    /**
-     * The color used for the track to the right of the button. Overrides the
-     * default blue gradient image.
-     */
-    maximumTrackTintColor: PropTypes.string,
-
-    /**
-     * The color used for the thumb.
-     */
-    thumbTintColor: PropTypes.string,
-
-    /**
-     * The size of the touch area that allows moving the thumb.
-     * The touch area has the same center has the visible thumb.
-     * This allows to have a visually small thumb while still allowing the user
-     * to move it easily.
-     * The default is {width: 40, height: 40}.
-     */
-    thumbTouchSize: PropTypes.shape(
-      {width: PropTypes.number, height: PropTypes.number}
-    ),
-
-    /**
-     * Callback continuously called while the user is dragging the slider.
-     */
-    onValueChange: PropTypes.func,
-
-    /**
-     * Callback called when the user starts changing the value (e.g. when
-     * the slider is pressed).
-     */
-    onSlidingStart: PropTypes.func,
-
-    /**
-     * Callback called when the user finishes changing the value (e.g. when
-     * the slider is released).
-     */
-    onSlidingComplete: PropTypes.func,
-
-    /**
-     * The style applied to the slider container.
-     */
-    style: ViewPropTypes.style,
-
-    /**
-     * The style applied to the track.
-     */
-    trackStyle: ViewPropTypes.style,
-
-    /**
-     * The style applied to the thumb.
-     */
-    thumbStyle: ViewPropTypes.style,
-
-    /**
-     * Sets an image for the thumb.
-     */
-    thumbImage: Image.propTypes.source,
-
-    /**
-     * Set this to true to visually see the thumb touch rect in green.
-     */
-    debugTouchArea: PropTypes.bool,
-
-    /**
-     * Set to true to animate values with default 'timing' animation type
-     */
-    animateTransitions : PropTypes.bool,
-
-    /**
-     * Custom Animation type. 'spring' or 'timing'.
-     */
-    animationType : PropTypes.oneOf(['spring', 'timing']),
-
-    /**
-     * Used to configure the animation parameters.  These are the same parameters in the Animated library.
-     */
-    animationConfig : PropTypes.object,
-
-    /**
-     * Used to determine the orientation of the slider. This allows for the orientation of the component to be both horizontal and vertical.
-     */
-    orientation: PropTypes.oneOf(['horizontal', 'vertical']),
-
-    /**
-     * Set this to true to invert the swipe direction of the slider. Inversion is linked to the slider's orientation.
-     */
-    inverted: PropTypes.bool,
-  };
-
   static defaultProps = {
     value: 0,
     minimumValue: 0,
